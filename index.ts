9c8ca63f4a486032f8a5c14749af997d4c3367b3
Matthew Hughes <42755431+zeepk@users.noreply.github.com>
2022-02-06 00:02:42 -0800

    order

--- index.ts
+++ index.ts
@@ -5,12 +5,12 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'client', 'build')));
 
-app.get('/*', (req, res) => {
-	res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-});
-
 app.get('/api/test', (req, res) => {
 	res.send('ok');
+});
+
+app.get('/*', (req, res) => {
+	res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
 });
 
 app.listen(process.env.PORT || 3000, () => {
