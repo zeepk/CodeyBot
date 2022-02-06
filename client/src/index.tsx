12a784f84e2f0ea1c026400a613de2b0eda07aa5
Matthew Hughes <42755431+zeepk@users.noreply.github.com>
2022-02-06 01:02:19 -0800

    build

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,13 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Auth0Provider } from '@auth0/auth0-react';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<Auth0Provider
+		domain={process.env.REACT_APP_AUTH_DOMAIN || ''}
+		clientId={process.env.REACT_APP_AUTH_ID || ''}
+		redirectUri={window.location.origin}
+	>
+		<React.StrictMode>
+			<App />
+		</React.StrictMode>
+	</Auth0Provider>,
+	document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
