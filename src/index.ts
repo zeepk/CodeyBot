a766a7b1737cf4f66d4990f052098bc6efb1012e
Matthew Hughes <42755431+zeepk@users.noreply.github.com>
2022-02-05 23:46:17 -0800

    proc

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import path from 'path';
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'client', 'build')));
+
+app.get('/*', (req, res) => {
+	res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+});
+
+app.get('/*', (req, res) => {
+	res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+});
+
+app.listen(process.env.PORT || 3000, () => {
+	console.log('The application is listening on port 3000!');
+});
