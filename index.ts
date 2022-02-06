cc61c62b8eb1783a3d3f224f234b097e71cef702
Matt Hughes <zeepkdb@gmail.com>
2022-02-05 22:45:32 -0800

    init

--- index.ts
+++ index.ts
@@ -1,9 +1,12 @@
 import express from "express";
+import path from "path";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Well done!");
+app.use(express.static(path.join(__dirname, "client", "build")));
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
 
 app.listen(3000, () => {
