695d0a58a0b4def67bdd244737f0f0f7330b1573
Matthew Hughes <42755431+zeepk@users.noreply.github.com>
2022-03-16 19:04:46 -0700

    req body

--- index.ts
+++ index.ts
@@ -1,12 +1,78 @@
 import express from 'express';
 import path from 'path';
+import dotenv from 'dotenv';
+import Discord, { TextChannel } from 'discord.js';
+import { json } from 'body-parser';
+var bodyParser = require('body-parser');
+dotenv.config();
 
+const client = new Discord.Client({
+	allowedMentions: {
+		parse: ['users', 'roles'],
+		repliedUser: true,
+	},
+	intents: ['GUILDS', 'GUILD_MESSAGES', 'GUILD_MESSAGE_REACTIONS'],
+});
+
+client.on('ready', () => {
+	console.log('CodeyBot running...');
+});
+
+client.login(process.env.BOT_TOKEN);
+
+// this listens for a message, usefull for !commands
+
+// client.on('messageCreate', async (message) => {
+// 	const content = message.content;
+// 	if (!content) {
+// 		return;
+// 	}
+// 	const response = await getResponse(content);
+// 	if (response) {
+// 		message.channel.send(response);
+// 	}
+// 	// if (content === 'test') {
+// 	// }
+// 	// if (content === '!commands') {
+// 	// 	message.channel.send(commands.commands);
+// 	// }
+
+// 	// if (content.indexOf('!villager ') === 0) {
+// 	// 	const searchTerm = content.split('!villager ')[1];
+// 	// 	message.channel.send(constants.nookipediaUrlPrefix + searchTerm);
+// 	// }
+// });
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'client', 'build')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(json());
 
 app.get('/api/test', (req, res) => {
 	res.send('ok');
+});
+
+app.post('/api/send', (req, res) => {
+	const { channelId, secret, message } = req.body;
+	console.log(channelId, secret, message);
+	if (!client || !client.channels || !client.channels.cache) {
+		const errorMessage = 'Error reading client channels';
+		console.error(errorMessage);
+		res.status(500).send(errorMessage);
+		return;
+	}
+
+	const channel = client.channels.cache.get(channelId) as TextChannel;
+	if (!channel) {
+		const errorMessage = `Error retrieving channel with id: ${channelId}`;
+		console.error(errorMessage);
+		res.status(500).send(errorMessage);
+		return;
+	}
+
+	channel.send(message);
+	res.send('success');
 });
 
 app.get('/*', (req, res) => {
